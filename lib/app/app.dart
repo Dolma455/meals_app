e0b74b5fd09a9e40c82fa7c68acc35179ee0a5ad
Dolma <dolma1918@gmail.com>
2023-12-07 13:25:00 +0545

    feat: Meals App created

---
+++ lib/app/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 186, 208, 232),
+    brightness: Brightness.dark,
+  ),
+  textTheme: GoogleFonts.poppinsTextTheme(),
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+    );
+  }
+}
