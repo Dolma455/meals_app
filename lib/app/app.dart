d405f05070eb158c71427037c28bf7e60afb74d4
Dolma <dolma1918@gmail.com>
2023-12-10 11:44:49 +0545

    feat: Meals data loaded intt a screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
-import 'package:expense_tracker/screen/categories.dart';
+import 'package:expense_tracker/data/dummy_data.dart';
+import 'package:expense_tracker/screen/meals.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -18,7 +19,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: MealsScreen(
+        title: 'some category',
+        meals: dummyMeals,
+      ),
     );
   }
 }
