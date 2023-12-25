7771cee6eb387da3a09a5f29ff5da7d4f24fef9e
Dolma <dolma1918@gmail.com>
2023-12-25 21:25:29 +0545

    feat: Adding a filter item

---
+++ lib/screen/filters.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class FilterScreen extends StatefulWidget {
+  const FilterScreen({super.key});
+
+  @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  var _gluttenFreeFilterSet = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _gluttenFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _gluttenFreeFilterSet = isChecked;
+              });
+            },
+            title: Text(
+              'GLuten-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include gluten-free meals.',
+              style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(
+              left: 34,
+              right: 22,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
