ce5ae171a75a2434c945b359f1178db2df12542d
Dolma <dolma1918@gmail.com>
2023-12-30 09:54:37 +0545

    feat: Animation Controller added

--- lib/screen/categories.dart
+++ lib/screen/categories.dart
@@ -5,7 +5,8 @@ import 'package:expense_tracker/screen/meals.dart';
 import 'package:expense_tracker/widgets/category_grid_item.dart';
 import 'package:flutter/material.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
+  
   const CategoriesScreen({
     super.key,
     required this.availableMeals,
@@ -13,8 +14,31 @@ class CategoriesScreen extends StatelessWidget {
 
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin{
+ late AnimationController _animationController;
+  @override
+  void initState() {
+    
+    super.initState();
+    _animationController=AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+      );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose(); //don't cause memory overflow
+    super.dispose();
+  }
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
